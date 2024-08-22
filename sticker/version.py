b579cd197c6ba9007df33c2f46b645ac871d3b8e
ghost <ghost@uu.com>
2024-08-22 15:09:16 +0800

    add pck

--- sticker/version.py
+++ sticker/version.py
@@ -1,1 +1,6 @@
-from .get_version import git_tag, git_revision, version, linkified_version
+# Generated in setup.py
+
+git_tag = None
+git_revision = '90558e34'
+version = '0.1.0+dev.90558e34'
+linkified_version = '0.1.0+dev.[90558e34](https://github.com/maunium/stickerpicker/commit/90558e34fe26d606c14804de2df4aea8c1257718)'
